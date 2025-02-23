152bef62243728c3466134539a3f12ae629f4eb0
Vaibhav <vaibhavsemwal5@gmail.com>
2025-02-23 19:18:38 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Layout } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+}
